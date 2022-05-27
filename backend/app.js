f962f37556893ed7c5ae82c862f69db0b4e2ad8d
Manish Thulung <manish.thulung58@gmail.com>
2022-05-27 23:50:52 +0545

    connect-src changed again

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", " true");
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://gadgetinfo-mern-stack.herokuapp.com/"
+    "gadgetinfo-mern-stack.herokuapp.com/"
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
