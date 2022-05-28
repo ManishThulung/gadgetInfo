74a7aa70503face9e21b78893b03e020f4d7f522
Manish Thulung <manish.thulung58@gmail.com>
2022-05-28 12:48:16 +0545

    set csp header connent-src changed again

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"],
-      connectSrc: ["'self'", "https://gadgetinfo-mern-stack.herokuapp.com/"],
+      connectSrc: ["'self'", "https://gadgetinfo-mern-stack.herokuapp.com"],
       scriptSrc: ["'self'", "'unsafe-inline'"],
       styleSrc: [
         "'self'",
